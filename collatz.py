46a279e2233bed70bb47d9fd685d0855c3dacea2
BenJGMay <41793709+BenJGMay@users.noreply.github.com>
2018-11-12 17:07:41 +0000

    Added files via upload

---
+++ collatz.py
@@ -0,0 +1,8 @@
+# If number is even should print number // 2 and return this value
+# If number is odd should print and return 3 * number + 1
+
+def collatz(number):
+    pass
+
+# Then write a program that lets the user type in an integer and that keeps
+# calling collatz() on that number until the function returns the value 1.
